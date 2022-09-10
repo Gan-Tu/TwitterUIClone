6ad9345c49e9d30251797ad9f551f4dd816fcd18
Gan Tu <github@tugan.dev>
2022-09-09 23:48:32 -0400

    Add comment box

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Tweet } from "../typings";
 import TimeAgo from "react-timeago";
 import { HeartIcon, ShareIcon, CommentIcon, RetweetIcon } from "./Icons";
+import toast from "react-hot-toast";
 
 interface Props {
   tweet: Tweet;
 }
 
 function Tweet({ tweet }: Props) {
+  const [commentBox, setCommentBox] = useState<boolean>(false);
+  const [input, setInput] = useState<string>("");
+
   return (
     <div className="flex flex-col space-x-3 p-5">
       <div className="flex space-x-3">
@@ -41,21 +45,50 @@ function Tweet({ tweet }: Props) {
         </div>
       </div>
 
-      <div className="mt-5 mr-10 flex justify-between px-10">
-        <div className="flex cursor-pointer items-center space-x-3 text-gray-500">
-          <HeartIcon className="h-5 w-5" />
+      <div className="mt-5 flex justify-between pl-10 pr-20">
+        <div
+          className="flex cursor-pointer  items-center  space-x-3 text-sm text-gray-500"
+          onClick={() => setCommentBox(!commentBox)}
+        >
+          <CommentIcon className="h-5 w-5" />
           <span>{tweet.userId}</span>
         </div>
-        <div className="flex cursor-pointer items-center space-x-3 text-gray-500">
+        <div className="flex cursor-pointer  items-center  space-x-3 text-sm text-gray-500">
+          <RetweetIcon className="h-5 w-5" />
+          <span>{tweet.userId * tweet.userId * 24}</span>
+        </div>
+        <div className="flex cursor-pointer  items-center  space-x-3 text-sm text-gray-500">
+          <HeartIcon className="h-5 w-5" />
+          <span>{tweet.userId * tweet.id * 8}</span>
+        </div>
+        <div className="flex cursor-pointer  items-center  space-x-3 text-sm text-gray-500">
           <ShareIcon className="h-5 w-5" />
         </div>
-        <div className="flex cursor-pointer items-center space-x-3 text-gray-500">
-          <CommentIcon className="h-5 w-5" />
-        </div>
-        <div className="flex cursor-pointer items-center space-x-3 text-gray-500">
-          <RetweetIcon className="h-5 w-5" />
-        </div>
       </div>
+
+      {commentBox && (
+        <form
+          className="mt-3 flex space-x-3 px-10"
+          onSubmit={(e) => {
+            e.preventDefault();
+            toast.success("Comment is posted.");
+          }}
+        >
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Write a comment..."
+            className="flex-1 rounded-lg bg-gray-100 p-2 outline-none"
+          />
+          <button
+            disabled={!input}
+            className="text-twitter disabled:text-gray-200"
+          >
+            Post
+          </button>
+        </form>
+      )}
     </div>
   );
 }
